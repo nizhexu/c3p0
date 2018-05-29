a9466e24a65958a2e6bd0636bd3752f3a0ff9021
nizhexu <279551882@qq.com>
2018-05-29 20:10:07 +0800

    Update BasicResourcePool.java

--- src/java/com/mchange/v2/resourcepool/BasicResourcePool.java
+++ src/java/com/mchange/v2/resourcepool/BasicResourcePool.java
@@ -1463,7 +1463,7 @@ class BasicResourcePool implements ResourcePool
                     _recheckResizePool();
 
                 this.wait(timeout);
-                if (timeout > 0 && System.currentTimeMillis() - start > timeout)
+                if (timeout > 0 && System.currentTimeMillis() - start >= timeout)
                     throw new TimeoutException("A client timed out while waiting to acquire a resource from " + this + " -- timeout at awaitAvailable()");
                 if (force_kill_acquires)
                     throw new CannotAcquireResourceException("A ResourcePool could not acquire a resource from its primary factory or source.", getLastAcquisitionFailure());
